59d62392692704d3cbdf38f5e86e9d70c25229e6
Benjamin <17WilsB@olab.org.uk>
2022-10-31 21:48:06 +0000

    Minor functionality improvements

--- Vector.py
+++ Vector.py
@@ -1,19 +1,23 @@
 from math import sqrt, acos, cos, sin
 class Vector(object):
     def fromAngle(m=0, a=0):
-        x = m*cos(a)
-        y = m*sin(a)
+        x = round(m*cos(a)*10000)/10000 # Removes most floating-point error
+        y = round(m*sin(a)*10000)/10000
         return Vector(x,y)
 
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+    def __init__(self, x=0, y=0, copy_vec=None):
+        if copy_vec is None:
+            self.x = x
+            self.y = y
+        else:
+            self.x = copy_vec.x
+            self.y = copy_vec.y
     
     # Define basic operations for velocities
 
     def __add__(a, b):
         if isinstance(b, Vector):
-            return Vector(a.x_vel + b.x_vel, a.y_vel + b.y_vel)
+            return Vector(a.x + b.x, a.y + b.y)
         return NotImplemented
     
     def __mul__(a, b):
@@ -26,6 +30,12 @@ class Vector(object):
     def __abs__(self):
         return sqrt(self.x**2 + self.y**2)
 
-    def findAngle(self, vec):
-        return acos((self*vec)/(abs(self)*abs(vec)))
-    
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+    def findAngle(self, vec=None):
+        if vec is None:
+            vec = Vector(1,0)
+        if abs(vec) != 0:
+            return acos((self*vec)/(abs(self)*abs(vec)))
+        return 0

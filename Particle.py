89589990a59348cb606ad47fa34097f5a859df5f
Benjamin <17WilsB@olab.org.uk>
2022-10-31 21:48:27 +0000

    Massive Particle Improvements

--- Particle.py
+++ Particle.py
@@ -1,9 +1,11 @@
 from Vector import Vector
+from math import pi
+
 class Particle(object):
 
     instances = []
 
-    def __init__(self, name="Unnamed", x=0, y=0, x_vel=0, y_vel=0, mass = 1):
+    def __init__(self, name="Unnamed", x=0, y=0, x_vel=0, y_vel=0, mass=1):
         Particle.instances.append(self)
         
         self.name = name
@@ -13,31 +15,48 @@ class Particle(object):
 
         self.vel = Vector(x_vel, y_vel)
 
+        self.vel_before_collision = Vector(copy_vec=self.vel)
+
         self.mass = mass
     
     def display(self):
-        print(f"Particle {self.name} is at coordinates ({self.x}, {self.y})")
+        print(f"Particle {self.name} is at coordinates ({self.x}, {self.y}), with velocity {self.vel}")
     
-    def move(self, x, y):
-        self.x += x
-        self.y += y
+    def move(self, vel):
+        self.x += vel.x
+        self.y += vel.y
     
     def checkCollision(self, collider):
+        #print(f"Collision check between {self.name} and {collider.name}: ({self.x}, {self.y}) & ({collider.x}, {collider.y}) -> {self.x == collider.x and self.y == collider.y}")
         if self.x == collider.x and self.y == collider.y:
             return True
     
-    def physicsUpdate(self):
-        self.move(self.x_vel, self.y_vel)
-        
-        collided = False
+    def collisionUpdate(self):
         for particle in Particle.instances:
-            if self.checkCollision(particle):
-                total_mass = self.mass + particle.mass
-                total_vel = self.vel + particle.vel
-                total_momentum = total_mass * total_vel
+            if particle is not self:
+                if self.checkCollision(particle):
+                    self_momentum_mag = abs(self.vel * self.mass)
+                    other_momentum_mag = abs(particle.vel_before_collision * particle.mass)
+                    total_momentum_mag = self_momentum_mag + other_momentum_mag
+                    new_angle = pi + self.vel.findAngle() - self.vel.findAngle(particle.vel_before_collision)
+                    new_magnitude = total_momentum_mag*self.mass/(self.mass + particle.mass)
+                    print(f"{self.name} colliding with {particle.name}:", round(10000*new_angle/pi)/10000, round(10000*self.vel.findAngle()/pi)/10000, round(10000*self.vel.findAngle(particle.vel_before_collision)/pi)/10000)
+                    self.vel = Vector.fromAngle(new_magnitude, new_angle)
                 
             
-if __name__ == "main":
-    a = Particle("Alpha")
-    b = Particle("Beta")
+#if __name__ == "main":
+from time import sleep
 
+a = Particle("Alpha", x=-1, y=-1, x_vel=1, y_vel=1)
+b = Particle("Beta", x=1, y=-1, x_vel=-1, y_vel=1)
+
+
+while True:
+    for particle in Particle.instances:
+        particle.display()
+        particle.move(particle.vel)
+    for particle in Particle.instances:
+        particle.vel_before_collision = Vector(copy_vec=particle.vel)
+        particle.collisionUpdate()
+    
+    sleep(0.5)
